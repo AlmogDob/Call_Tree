cf676800e5266fd98626187cff05ae78525d3426
Almog Dobrescu <almogd@campus.technion.ac.il>
2026-06-12 22:26:11 +0300

    [adding basic librarys

--- src/call_tree.c
+++ src/call_tree.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define ALMOG_LEXER_IMPLEMENTATION
+#define ALMOG_STRING_MANIPULATION_IMPLEMENTATION
+#include "Almog_Lexer.h"
+
 int main(int argc, char const *argv[])
 {
     printf("Hello, World!\n");
